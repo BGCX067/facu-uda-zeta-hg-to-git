b0b741d3a76090058ed30e329614edc22b666d31
zetahernandez <zetahernandez@gmail.com>
2012-07-01 02:30:53 -0300

    c

--- config.php
+++ config.php
@@ -4,5 +4,6 @@ $include_path = ini_get('include_path');
 $include_path.= getcwd().DIRECTORY_SEPARATOR.'model';
 $include_path.= getcwd().DIRECTORY_SEPARATOR.'model'.DIRECTORY_SEPARATOR.'database';
 $include_path.= getcwd().DIRECTORY_SEPARATOR.'controller';
+$include_path.= getcwd().DIRECTORY_SEPARATOR.'view|';
 ini_set('include_path',$include_path);
 ?>
